2c8df535c3e11a4fd6851e93807f149555b3761b
Matt Erhart <mattjerhart@gmail.com>
2017-06-09 16:25:22 -0700

    IT BEGINS

---
+++ src/entryApp/index.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { Router, Route, Switch } from "react-router";
+import { createBrowserHistory } from "history";
+import { configureStore } from "../redux/configureStore";
+import { App } from "./App";
+
+const store = configureStore();
+const history = createBrowserHistory();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={history}>
+      <Switch>
+        <Route path="/" component={App} />
+      </Switch>
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
